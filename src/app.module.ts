22b959a2c6627c98091b14b8f0ead92ab9609fd7
romigentile <romigentile@hotmail.com>
2024-06-25 20:35:05 -0300

    Se configura app.module para conectar la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,18 @@ import { SedeModule } from './sede/sede.module';
 import { CanchaModule } from './cancha/cancha.module';
 import { TurnoModule } from './turno/turno.module';
 import { AuthModule } from './auth/auth.module';
+import typeOrmConfig from './config/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [UserModule, SedeModule, CanchaModule, TurnoModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true, load: [typeOrmConfig]}),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory:(ConfigService: ConfigService) => ConfigService.get("typeorm")}), 
+    UserModule, SedeModule, CanchaModule, TurnoModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
