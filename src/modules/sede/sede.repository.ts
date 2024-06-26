2c3032ff6d687cfe2aa5b751f6c45543e45e3820
romigentile <romigentile@hotmail.com>
2024-06-26 18:07:42 -0300

    Se configura la precarga de sedes

--- src/modules/sede/sede.repository.ts
+++ src/modules/sede/sede.repository.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { Venue } from './sede.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as data from '../../common/precarga/sedes.json';
 import { UUID } from 'crypto';
 import { CreateSedeDto } from './dto/createSede.dto';
 @Injectable()
@@ -11,12 +10,6 @@ export class SedeRepository {
     @InjectRepository(Venue)
     private sedeRepository: Repository<Venue>,
   ) { }
-
-  // async onModuleInit() {
-  //   if ((await this.sedeRepository.find()).length == 0) {
-  //     await this.addSedesDefault();
-  //   }
-  // }
 
   async getSedes(): Promise<Venue[]> {
     return await this.sedeRepository.find();
@@ -39,18 +32,4 @@ export class SedeRepository {
     await this.sedeRepository.delete(id);
     return 'Sede deleted';
   }
-
-  //! SI ROMI NO LO UTILIZA SE BORRA
-  // async addSedesDefault() {
-  //   for (const element of data) {
-  //     await this.sedeRepository
-  //       .createQueryBuilder()
-  //       .insert()
-  //       .into(Venue)
-  //       .values(element)
-  //       .orUpdate(['description', 'location', 'imgUrl'], ['name'])
-  //       .execute();
-  //   }
-  //   return 'Precarga de sedes realizada con exito.';
-  // }
 }
