5ee264c62cdac03a7eb67906b6aae2d3e2265cfb
Javierc97 <javi.c97@hotmail.com>
2024-06-26 16:47:16 -0300

    se agrega repository al modulo de canchas y clas validator

--- src/modules/cancha/cancha.module.ts
+++ src/modules/cancha/cancha.module.ts
@@ -3,10 +3,11 @@ import { CanchaController } from './cancha.controller';
 import { CanchaService } from './cancha.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Court } from './cancha.entity';
+import { canchaRepository } from './cancha.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Court])],
   controllers: [CanchaController],
-  providers: [CanchaService]
+  providers: [CanchaService, canchaRepository],
 })
 export class CanchaModule {}
