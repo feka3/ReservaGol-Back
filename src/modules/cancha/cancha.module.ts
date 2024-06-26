9974a806b1707e6b482e82cfc41ca2622e6b01db
romigentile <romigentile@hotmail.com>
2024-06-26 15:03:55 -0300

    Se agregan imports a los modulos

--- src/modules/cancha/cancha.module.ts
+++ src/modules/cancha/cancha.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CanchaController } from './cancha.controller';
 import { CanchaService } from './cancha.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Court } from './cancha.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Court])],
   controllers: [CanchaController],
   providers: [CanchaService]
 })
