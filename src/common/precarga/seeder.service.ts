38e5779863f88b54e9179e5e3d0bae236606e582
romigentile <romigentile@hotmail.com>
2024-06-27 20:18:40 -0300

    cambio de nombre al archivo de precarga por data.json para que sea general (sedes + canchas)

--- src/common/precarga/seeder.service.ts
+++ src/common/precarga/seeder.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { SedeRepository } from 'src/modules/sede/sede.repository';
-import * as data from '../../common/precarga/sedes.json';
+import * as data from '../../common/precarga/data.json';
 
 
 @Injectable()
