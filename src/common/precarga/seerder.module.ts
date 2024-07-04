ad321dc23dd1a1ee05c1320f39182b5ad1069b90
romigentile <romigentile@hotmail.com>
2024-07-03 23:32:47 -0300

    MAIL PARA CONFIRMACION DE REGISTRO DE USER

--- src/common/precarga/seerder.module.ts
+++ src/common/precarga/seerder.module.ts
@@ -8,9 +8,10 @@ import { Cancha } from 'src/modules/cancha/cancha.entity';
 import { User } from 'src/modules/user/user.entity';
 import { UserRepository } from 'src/modules/user/user.repository';
 import { AuthService } from 'src/modules/auth/auth.service';
+import { EmailService } from 'src/modules/email/email.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Sede, Cancha, User])],
-  providers: [SedeRepository, CanchaRepository, UserRepository, SeederService, AuthService],
+  providers: [SedeRepository, CanchaRepository, UserRepository, SeederService, AuthService, EmailService],
 })
 export class SeederModule { }
