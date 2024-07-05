2850986d8f8bad9a57eb5f282f937be84bf54492
romigentile <romigentile@hotmail.com>
2024-07-04 21:11:26 -0300

    SWAGGER USER CONTROLLER

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,6 @@
 
 import { Body, Controller, Get, Param, ParseUUIDPipe, Put, UploadedFile } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-
-
 import { UserService } from './user.service';
 import { User } from 'mercadopago';
 import { CloudinaryService } from '../cloudinary/cloudinary.service';
@@ -14,17 +12,32 @@ export class UserController {
     constructor(
         private readonly userService: UserService,
         private readonly cloudinaryService: CloudinaryService) {}
-
+    
+    /**
+     * Petición para obtener el listado de todos los usuarios que se encuentran en la base de datos.
+     * 
+     */
     @Get()
     async getUsers() {
         return this.userService.getUsers()
     }
-        
+
+    /**
+     * Petición para obtener los datos de un usuario que se encuentra en la base de datos.
+     * - Se requiere enviar por parámetro el ID del usuario.
+     * 
+     */        
     @Get(":id")
     async getUserById(@Param("id") id: string) {
         return await this.userService.getUserById(id);
     }
 
+    /**
+     * Petición para madificar los datos de un usuario que se encuentra en la base de datos.
+     * - Se requiere enviar por parámetro el ID del usuario.
+     * - No es necesario enviar todos los datos, solo los que desea modificar.
+     * - Se puede cargar una imagen de perfil.
+     */ 
     @Put(":id")
     async updateUserById(@Param("id", ParseUUIDPipe) id: string, @Body() user: Partial<User>, @UploadedFile() file: Express.Multer.File) {
         const uploadResult = await this.cloudinaryService.uploadImage(file);
